a4c7b4a0b4ce520eb9b4f25afa40233fe7d605bc
Hebert Rocha <hebertrocha76@gmail.com>
2023-09-27 21:22:40 -0300

    feat: create home screen

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -5,6 +5,8 @@ import { StatusBar } from "expo-status-bar";
 import * as Icon from "react-native-feather";
 import {themeColors} from "../theme";
 import Categories from "../components/categories";
+import {featured} from "../constants";
+import FeaturedRow from "../components/featuredRow";
 export default function HomeScreen() {
     return (
         <SafeAreaView class="bg-white">
@@ -34,6 +36,22 @@ export default function HomeScreen() {
                 {/*categories*/}
 
                 <Categories />
+
+                {/*featured*/}
+                <View className="mt-5">
+                    {
+                        [featured, featured, featured].map((item, index) => {
+                            return (
+                                <FeaturedRow
+                                    key={index}
+                                    title={item.title}
+                                    restaurants={item.restaurants}
+                                    description={item.description}
+                                />
+                            )
+                        })
+                    }
+                </View>
             </ScrollView>
         </SafeAreaView>
     );
